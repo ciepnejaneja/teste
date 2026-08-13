22e0c910013e05ddbb2d4af3701f6ead59c4e3cc
Josimar <ciepnejaneja@gmail.com>
2026-08-13 14:06:21 -0300

    Adiciona arquivos da aula

---
+++ up.cmd
@@ -0,0 +1,15 @@
+echo "# teste" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ciepnejaneja/teste.git
+git push -u origin main
+
+GOTO END
+git remote add origin https://github.com/ciepnejaneja/teste.git
+git branch -M main
+git push -u origin main
+
+:END
+
---
+++ variaveis.php
@@ -0,0 +1,18 @@
+<h1>
+<?php
+$nome = "Maria";
+$idade = 25;
+$altura = 1.68;
+
+
+
+var_dump($nome);
+echo "<br>";
+var_dump($altura);
+echo "<br>";
+
+echo "Nome : $nome Idade :
+ $idade, Altura : $altura" ;
+
+
+?>
